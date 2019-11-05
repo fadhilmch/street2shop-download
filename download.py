74aef2cfe3ce255eeb6b696c36876a66c39c27fa
Fadhilmch <fadhil.mochammad1095@gmail.com>
2019-11-05 15:08:09 +0100

    use ThreadPoolExecutor

--- download.py
+++ download.py
@@ -1,43 +1,50 @@
+#!/usr/bin/python
+
 from tomorrow import threads
 import os
 import sys
 import logging
 import urllib.request
 import json
+from concurrent.futures.thread import ThreadPoolExecutor
 
-    
-def main(args):
-    
-    # Run the download function on multi-threads
-    @threads(args.threads)
-    def download(item_id, url, images_dir):
-        if not os.path.exists(images_dir):
-            os.makedirs(images_dir)
-        try:
-            file_output = os.path.join(images_dir, str(item_id) + '.' + 'JPEG')
-            urllib.request.urlretrieve(url, file_output)
-            print(file_output)
-        except:
-            print("Unexpected error:", sys.exc_info()[0])
-            logging.error(sys.exc_info()[0])
-    
-    # Download images for each class
-    def read_class(class_name, max_num_samples, url_dict, images_dir):
-        print('Downloading images for class ' + class_name + '...')
-        file_loc = 'meta/json/train_pairs_' + class_name + '.json'
-        file_json = open(file_loc, 'r')
-        meta_data = json.load(file_json)
-        file_json.close()
 
-        for i,data in enumerate(meta_data):
-            if i >= max_num_samples:
-#                 print('Downloaded ' + str(len(next(os.walk(output_dir))[2])) + ' images for class ' + class_name)
-                break
-            photo_id = int(data['photo'])
-            url = url_dict[photo_id]
-            output_dir = os.path.join(images_dir, class_name)
-            download(photo_id, url, output_dir)
+# download function on multi-threads
+def download(item_id, url, images_dir):
+    if not os.path.exists(images_dir):
+        os.makedirs(images_dir)
+    try:
+        file_output = os.path.join(images_dir, str(item_id) + '.' + 'JPEG')
+        urllib.request.urlretrieve(url, file_output)
+        print(file_output)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        logging.error(sys.exc_info()[0])
+
+# Download images for each class
+def read_class(class_name, max_num_samples, url_dict, images_dir, threads):
+    print('Downloading images for class ' + class_name + '...')
+    file_loc = 'meta/json/train_pairs_' + class_name + '.json'
+    file_json = open(file_loc, 'r')
+    meta_data = json.load(file_json)
+    file_json.close()
+    images_list = []
+
+    for i,data in enumerate(meta_data):
+        if i >= max_num_samples:
+            break
+        photo_id = int(data['photo'])
+        url = url_dict[photo_id]
+        output_dir = os.path.join(images_dir, class_name)
+        images_list.append({'item_id': photo_id, 'url': url, 'images_dir': output_dir})
+        if i % args.threads == 0:
+            with ThreadPoolExecutor(max_workers=args.threads) as executor:
+                for x in images_list:
+                    executor.submit(download, x['item_id'], x['url'], x['images_dir'])
+                images_list = []
+    print('Downloaded ' + str(len(next(os.walk(output_dir))[2])) + ' images for class ' + class_name)
     
+def main(args):    
     print('Start downloading images from Street2Shop dataset...')
     
     if args.log is not None:
@@ -52,17 +59,21 @@ def main(args):
     # Check whether we want to download all images or for specified class only
     if(args.classes[0] == 'all'):
         print('Downloading all images...')
+        images_list = []
         for i, (item_id, url) in enumerate(url_dict.items()):
-            print(i)
             if i >= args.max_num_samples:
-#                 print(i,len(url_dict),args.max_num_samples)
-#                 print('Downloaded ' + str(len(next(os.walk(args.images_dir))[2])) + ' images')
-#                 print('Finished')
                 break
-            download(item_id, url, args.images_dir)
+            images_list.append({'item_id': item_id, 'url': url, 'images_dir': args.images_dir})
+            if i % args.threads == 0:
+                with ThreadPoolExecutor(max_workers=args.threads) as executor:
+                    for x in images_list:
+                        executor.submit(download, x['item_id'], x['url'], x['images_dir'])
+                    images_list = []
+        print('Downloaded ' + str(len(next(os.walk(args.images_dir))[2])) + ' images')
+            
     else:
         for class_name in args.classes:
-            read_class(class_name, args.max_num_samples, url_dict, args.images_dir)
+            read_class(class_name, args.max_num_samples, url_dict, args.images_dir, args.threads)
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
@@ -79,6 +90,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     main(args)
-        
+    print('Finished')
+    
     exit(0)
     
