d2e789bffb2860591de726f639f64d5626504d49
fadhilmch <fadhil.mochammad@traveloka.com>
2024-10-16 22:20:21 +0700

    feat: add config file

---
+++ config.py
@@ -0,0 +1,15 @@
+DEFAULT_CLASSES = [
+    "bags",
+    "belts",
+    "dresses",
+    "eyewear",
+    "footwear",
+    "hats",
+    "leggings",
+    "outerwear",
+    "pants",
+    "skirts",
+    "tops",
+]
+DEFAULT_IMAGE_DIR = "images"
+DEFAULT_THREADS = 10
